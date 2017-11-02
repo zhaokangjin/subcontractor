fe19ad97415fbaae3aadd25fde9c96b77983815d
zhaokangjin <kangjin.zhao@accenture.com>
2017-11-02 21:10:15 +0800

    公众号开发

---
+++ subcontractor/src/main/java/com/accenture/subcontractor/job/common/util/constant/Constant.java
@@ -0,0 +1,7 @@
+package com.accenture.subcontractor.job.common.util.constant;
+public class Constant {
+
+	public static final String AppId = "wx1a7ae5e384aa9ca2";
+	public static final String Secret = "88a2fd9c101507007e3fb66c0e206317";
+	
+}
